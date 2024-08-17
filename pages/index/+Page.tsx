d510c8d75c867f4d5b048d49daa3cb72e2e44f28
snake-py <fabio@snake-py.com>
2024-08-17 13:32:08 +0200

    added suggested workaround

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,18 +1,19 @@
-export { Page }
+export { Page };
 
-import { Counter } from './Counter'
+import { Counter } from './Counter';
+import classes from './test.module.css';
 
 function Page() {
-  return (
-    <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-    </>
-  )
+    return (
+        <>
+            <h1 className={classes.test}>Welcome</h1>
+            This page is:
+            <ul>
+                <li>Rendered to HTML.</li>
+                <li>
+                    Interactive. <Counter />
+                </li>
+            </ul>
+        </>
+    );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
-import react from '@vitejs/plugin-react'
-import vike from 'vike/plugin'
-import { UserConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import vike from 'vike/plugin';
+import { UserConfig } from 'vite';
+// import { patchCssModules } from 'vite-css-modules';
 
 const config: UserConfig = {
-  plugins: [react(), vike()]
-}
+    plugins: [
+        {
+            name: 'hack-css-module-hmr',
+            handleHotUpdate(ctx) {
+                // prevent full reload due to non self-accepting css module.
+                // here only "?direct" module should be filtered out as it doesn't have a parent module.
+                if (ctx.file.includes('module.css')) {
+                    return ctx.modules.filter((m) => !m.id?.includes('?direct'));
+                }
+            },
+        },
+        react(),
+        vike(),
+    ],
+};
 
-export default config
+export default config;
